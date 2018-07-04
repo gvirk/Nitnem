9705ca6274c9e87b46fe35bd2a1e62e758fda530
Gursharnbir Singh <gursharnbirsingh@gursharnbirs-macbook-pro.local>
2018-07-04 10:29:51 -0600

    Initial Commit

---
+++ Nitnem/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Nitnem
+//
+//  Created by Gursharnbir Singh on 2018-07-04.
+//  Copyright © 2018 Gursharnbir Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
